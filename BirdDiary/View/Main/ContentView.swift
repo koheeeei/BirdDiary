f243ab6839d4135773a48523b777497bc03c707b
A <A>
2025-03-25 21:24:50 +0900

    testです。

--- BirdDiary/View/Main/ContentView.swift
+++ BirdDiary/View/Main/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Button("メモを追加") {
+                Button("テストメモを追加") {
                     showPopup.toggle()
                 }
                 .padding()
